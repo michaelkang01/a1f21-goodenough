9d6fe199da01da3ffe645762565bee62ac5287b7
michaelkang01 <44816025+michaelkang01@users.noreply.github.com>
2021-10-12 19:39:35 -0400

    Returns ids instead of names, prepare for bacon

--- src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
+++ src/main/java/ca/utoronto/utm/mcs/Neo4jDAO.java
@@ -63,14 +63,14 @@ public class Neo4jDAO {
             String query = "MATCH (a:actor) WHERE a.id = \"%s\" RETURN a.id, a.Name";
             query = String.format(query, actorID);
             Result result = session.run(query);
-            String query2 = "MATCH (actor {id: \"%s\"})--(m:movie) RETURN m.Name";
+            String query2 = "MATCH (actor {id: \"%s\"})--(m:movie) RETURN m.id";
             query2 = String.format(query2, actorID);
             Result result2 = session.run(query2);
             Record actRec = result.next();
             ArrayList<String> movielist = new ArrayList<String>();
             while (result2.hasNext()) {
                 Record rec = result2.next();
-                movielist.add(rec.get("m.Name").asString());
+                movielist.add(rec.get("m.id").asString());
             }
             toRet.put("actorId", actRec.get("a.id").asString());
             toRet.put("name", actRec.get("a.Name").asString());
@@ -85,14 +85,14 @@ public class Neo4jDAO {
             String query = "MATCH (m:movie) WHERE m.id = \"%s\" RETURN m.id, m.Name";
             query = String.format(query, movieID);
             Result result = session.run(query);
-            String query2 = "MATCH (movie {id: \"%s\"})--(a:actor) RETURN a.Name";
+            String query2 = "MATCH (movie {id: \"%s\"})--(a:actor) RETURN a.id";
             query2 = String.format(query2, movieID);
             Result result2 = session.run(query2);
 
             ArrayList<String> actorlist = new ArrayList<String>();
             while (result2.hasNext()) {
                 Record rec = result2.next();
-                actorlist.add(rec.get("a.Name").asString());
+                actorlist.add(rec.get("a.id").asString());
             }
             Record actRec = result.next();
             toRet.put("movieId", actRec.get("m.id").asString());
@@ -117,11 +117,19 @@ public class Neo4jDAO {
         return toRet.toString();
     }
 
-    public void computeBaconNumber(String actorID) throws JSONException{
-        return;
+    public String computeBaconNumber(String actorID) throws JSONException{
+        JSONObject toRet = new JSONObject();
+        try (Session session = this.driver.session() ) {
+            int pathCount = -1;
+        }
+        return toRet.toString();
     }
 
-    public void computeBaconPath(String actorID) throws JSONException{
-        return;
+    public String computeBaconPath(String actorID) throws JSONException{
+        JSONObject toRet = new JSONObject();
+        try (Session session = this.driver.session() ) {
+            ArrayList<String> path = new ArrayList<String>();
+            }
+        return toRet.toString();
     }
 }
