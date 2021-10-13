fd1a9e0ab1094e952d304a6262e7fb06ad5c8611
michaelkang01 <44816025+michaelkang01@users.noreply.github.com>
2021-10-13 16:01:41 -0400

    ReqHandler module, component setup

--- src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
+++ src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
@@ -3,10 +3,17 @@ package ca.utoronto.utm.mcs;
 import java.io.IOException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import javax.inject.Inject;
 
 public class ReqHandler implements HttpHandler {
 
     // TODO Complete This Class
+    private Neo4jDAO neo4j;
+
+    @Inject
+    public ReqHandler(Neo4jDAO neo4j) {
+        this.neo4j = neo4j;
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
