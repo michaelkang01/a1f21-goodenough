e97ecb0dbe1875f902ecddcfb3cd31fa2bc040b3
michaelkang01 <44816025+michaelkang01@users.noreply.github.com>
2021-10-14 00:07:58 -0400

    Server Dagger Injection working.

--- src/main/java/ca/utoronto/utm/mcs/Server.java
+++ src/main/java/ca/utoronto/utm/mcs/Server.java
@@ -1,5 +1,15 @@
 package ca.utoronto.utm.mcs;
+import com.sun.net.httpserver.HttpServer;
+import javax.inject.Inject;
+import java.io.IOException;
 
 public class Server {
     // TODO Complete This Class
+    public HttpServer hts;
+
+    @Inject
+    public Server(HttpServer hts) throws IOException {
+        this.hts = hts;
+    }
+
 }
